ad04402c250af85a05a8e832ad829d1ba4a3cdea
0xh7ml <0xh7ml.py@gmail.com>
2025-05-09 09:26:50 +0600

    Update settings to use environment variables for DEBUG and allow all hosts

--- core/settings.py
+++ core/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0kyhvdbat&$aa&x-%yqjt@_^v4ll-ujfe%)1ofr!pf@6qw$z3j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
